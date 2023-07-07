c6fcd6beb994ccdf3c12fa4a48e011c1e8ea97ce
btxdev <limecompro@gmail.com>
2023-07-07 09:40:26 +0500

    feat: implement canvas auto-resize

--- src/game/Canvas.ts
+++ src/game/Canvas.ts
@@ -1,17 +1,33 @@
 export default class Canvas {
     public id: string;
-    public width: number;
-    public height: number;
+    private _width: number;
+    private _height: number;
     public element: HTMLCanvasElement;
     
     constructor(id: string, width: number, height: number) {
         this.id = id;
-        this.width = width;
-        this.height = height;
+        this._width = width;
+        this._height = height;
         const canvas: HTMLCanvasElement = document.createElement('canvas')
         canvas.setAttribute('id', this.id)
         canvas.setAttribute('width', String(this.width))
         canvas.setAttribute('height', String(this.height))
+        canvas.setAttribute('class', 'canvas')
         this.element = canvas
+    }
+
+    set width(value: number) {
+        this._width = value;
+        this.element.setAttribute('width', String(value))
+    }
+    get width(): number {
+        return this._width;
+    }
+    set height(value: number) {
+        this._height = value;
+        this.element.setAttribute('height', String(value))
+    }
+    get height(): number {
+        return this._height;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@ import './styles/main.css'
 import Canvas from './game/Canvas.ts'
 import Game from './game/Game.ts'
 
-const canvas = new Canvas('canvas', screen.width, screen.height)
+const canvas = new Canvas('canvas', document.body.clientWidth, document.body.clientHeight)
+canvas.element.style.position = 'absolute';
+canvas.element.style.top = '0';
+canvas.element.style.left = '0';
 document.querySelector<HTMLDivElement>('#app')?.appendChild(canvas.element)
+window.addEventListener('resize', (e: UIEvent) => {
+    const element = e.target as Window
+    canvas.width = element.innerWidth || 640
+    canvas.height = element.innerHeight || 480
+})
 
 const game = new Game(canvas.element)
